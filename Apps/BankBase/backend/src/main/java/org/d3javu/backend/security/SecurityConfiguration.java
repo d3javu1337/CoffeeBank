7a16e3b33d4702d08e41f31b492079747a9bb3e2
egor <dejja1337@gmail.com>
2025-03-06 22:41:49 +0500

    init base backend

---
+++ Apps/BankBase/backend/src/main/java/org/d3javu/backend/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package org.d3javu.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+
+
+}
--- README.md
+++ README.md
@@ -12,3 +12,14 @@
 
 ## Tech stack : 
     will be later
+
+### The assignment of responsibilities:
+#### bank core:
+    logic of all operations with bank user`s money, like transfers, withdraws and etc.
+
+#### bank base:
+    provide all actions for user, like registration, transfers, etc.
+
+#### bank business:
+    provides all actions for business user - registration of legal entity in bank system, and other
+
